62afec14d9f4fc728eadd5c73ab93a5c8d6f53ee
smithraphael <smithraphael@hotmail.com>
2015-01-20 15:27:59 -0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The makeCacheMatrix enclosure 4 functions described below
+## set():
+## get():
+##setsolve():
+##getsolve():
 
 makeCacheMatrix <- function(x = matrix()) {
+
+        s=NULL
+        
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+
 
 }
 
